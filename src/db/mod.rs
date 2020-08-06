1ab2e9e138bcf1fe192512e207dfd1a750d1c41a
Bohdan Khorolets <b@khorolets.com>
2020-08-06 19:09:44 +0300

    refactor: Extend README with some initial information (#11)
    * Extend README, update main() to support config init and run. Small clean up of the code
    
    * Address some of the review suggestions
    
    * Move log about NEAR Indexer for Wallet is started to better place

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,4 +1,21 @@
+use std::env;
+
+use diesel::r2d2::{ConnectionManager, Pool};
+use diesel::PgConnection;
+use dotenv::dotenv;
+
 pub(crate) mod access_keys;
 pub(crate) mod enums;
 
 pub(crate) use access_keys::AccessKey;
+
+pub(crate) fn establish_connection() -> Pool<ConnectionManager<PgConnection>> {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL")
+        .unwrap_or_else(|_| panic!("DATABASE_URL must be set in .env file"));
+    let manager = ConnectionManager::<PgConnection>::new(&database_url);
+    Pool::builder()
+        .build(manager)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
