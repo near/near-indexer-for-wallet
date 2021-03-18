80ae43671da58014ab214fa154a05ddfeaedf9e7
Bohdan Khorolets <bogdan@khorolets.com>
2021-03-18 12:12:51 +0200

    fmt

--- src/state_viewer.rs
+++ src/state_viewer.rs
@@ -113,7 +113,11 @@ pub(crate) fn state_dump(
         for item in trie {
             let (key, value) = item.unwrap();
             if let Some(mut sr) = StateRecord::from_raw_key_value(key, value) {
-                if let StateRecord::Account { account_id, account } = &mut sr {
+                if let StateRecord::Account {
+                    account_id,
+                    account,
+                } = &mut sr
+                {
                     if account.locked() > 0 {
                         let stake = *validators.get(account_id).map(|(_, s)| s).unwrap_or(&0);
                         account.set_amount(account.amount() + account.locked() - stake);
